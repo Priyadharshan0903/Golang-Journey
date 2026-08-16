164c8da55945fc69cbd9c2e2333939bff35e4575
Priyadharshan0903 <priyadharshansenthil@gmail.com>
2026-08-16 07:14:47 +0530

    Add exercises for arrays, slices, and concurrency

---
+++ exercises/Arrays_Slices/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// multiple ways to initialize any array
+	// using make and just by declaring them in the curly bracket with/without denoting the size of it
+	var sample = [3]string{"hello", "mosi mosi", "konichiwa"}
+
+	fmt.Println(sample)
+}
